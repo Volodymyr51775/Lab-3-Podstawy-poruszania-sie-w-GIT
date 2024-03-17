802603f9d614a57ccfe397b4a5848b40c8f7fc66
Volodymyr <dsw51775@student.dsw.edu.pl>
2024-03-17 15:09:04 +0200

    Modified scrypt.sh to the second part of the labaratorium

--- scrypt.sh
+++ scrypt.sh
@@ -11,22 +11,45 @@ create_log(){
 
 show_help(){
     echo "Dostepne opcje:"
-    echo "--date wyswietli dzisiejsza date"
-    echo "--logs utworzy automatycznie 100 plikow logx.txt, x numer pliku od 1 100"
-    echo "--logs 30 utworzenie automatycznie 30 plikow, analogicznie jak wyzej, ale z obsluga argumentu liczby plikow"
-    echo "--help wyswietlenie wszystkich dostepnych opcji"
+    echo "--date, -d: wyswietli dzisiejsza date"
+    echo "--logs, -l: utworzy automatycznie 100 plikow logi.txt, i numer pliku od 1 do 100"
+    echo "--logs 30, -l 30: utworzenie automatycznie 30 plikow, analogicznie jak wyzej, ale z obsluga argumentu liczby plikow"
+    echo "--help, -h: wyswietlenie wszystkich dostepnych opcji"
+    echo "--init, -i: klonuje repozytorium"
+    echo "--error, -e: tworzy errorx/errorx.txt"
+}
+
+init_repo() {
+    git clone https://github.com/Volodymyr51775/Lab-4-Podstawa-praca-z-GIT.git
+    repo_name=$(basename "https://github.com/Volodymyr51775/Lab-4-Podstawa-praca-z-GIT.git" .git)
+    export PATH=$PATH:$(pwd)/$repo_name
+    echo "Repozytorium $repo_name sklonowane i dodane do PATH"
+}
+
+create_errors(){
+    mkdir -p errorx
+    for i in $(seq 1 ${1:-100}); do
+        echo "error$i.txt - utworzony przez skrypt skrypt.sh" > "errorx/error$i.txt"
+    done
 }
 
 case "$1" in
-    --date)
+    --date|-d)
     show_date
     ;;
-    --logs)
+    --logs|-l)
     count=${2:-100}
     create_log $count
     ;;
-    --help)
+    --help|-h)
     show_help
+    ;;
+    --error|-e)
+    count=${2:-100}
+    create_errors $count
+    ;;
+    --init|-i)
+    init_repo
     ;;
     *)
     echo "Nieznana opcja: $1"
